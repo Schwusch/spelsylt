f395853104272d863848aa391118062541000127
Jonathan Böcker <jonathan.e.m.bocker@gmail.com>
2018-11-04 18:07:55 +0100

    player

---
+++ assets/Player/idle.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="idle" tilewidth="100" tileheight="100" tilecount="4" columns="4">
+ <image source="../idle.png" trans="5e81a2" width="400" height="100"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
